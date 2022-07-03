baaca9711bd381f3b575e4d1d8dfa28575d6e423
bigJIU <1572204178@qq.com>
2022-07-03 12:43:31 +0800

    Update IFuncObject.cs

--- Assets/Scripts/Interaction/IFuncObject.cs
+++ Assets/Scripts/Interaction/IFuncObject.cs
@@ -1,14 +1,81 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class IFuncObject
+public class IFuncObject: MonoBehaviour
 {
-    public Action<GameObject, GameObject> interact;
+    [Header("Attach")] 
+    public List<UnityEvent> shadowAttach;
+    public List<UnityEvent> humanAttach;
+    public List<UnityEvent> otherAttach;
+    
+    [Header("Enter")] 
+    public List<UnityEvent> shadowEnter;
+    public List<UnityEvent> humanEnter;
+    public List<UnityEvent> otherEnter;
+    
+    [Header("Exit")] 
+    public List<UnityEvent> shadowExit;
+    public List<UnityEvent> humanExit;
+    public List<UnityEvent> otherExit;
 
-    public void doFunc(GameObject a,GameObject b)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        interact?.Invoke(a,b);
+        if (col.CompareTag("SO"))
+        {
+            foreach (var var in shadowEnter)
+            {
+                var?.Invoke();
+            }
+        }
+        else if (col.CompareTag("LO"))
+        {
+            foreach (var var in humanEnter)
+            {
+                var?.Invoke();
+            }
+        }
+        else
+        {
+            foreach (var var in otherEnter)
+            {
+                var?.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("SO"))
+        {
+            foreach (var var in shadowExit)
+            {
+                var?.Invoke();
+            }
+        }
+        else if (other.CompareTag("LO"))
+        {
+            foreach (var var in humanExit)
+            {
+                var?.Invoke();
+            }
+        }
+        else
+        {
+            foreach (var var in otherExit)
+            {
+                var?.Invoke();
+            }
+        }
+    }
+
+
+    //TODO: Just an example
+    public void fadeSelf(float time)
+    {
+        StartCoroutine("Fade");
     }
 }
